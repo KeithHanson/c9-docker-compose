e85c94a6eddace7a04cde4cf338010dc884ac720
Keith Hanson <keith@keithhanson.io>
2021-01-26 22:35:02 -0600

    dot env ALL THE THINGS

--- refresh-snapshot.multi.sh
+++ refresh-snapshot.multi.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
+source .env
+echo $SNAPSHOT_URL
 
 echo " ->Creating a clean environment (delete/start/stop containers)"
-docker-compose down -v
-docker-compose up -d
-docker-compose stop
+docker-compose -f docker-compose.multi.yml down -v
+docker-compose -f docker-compose.multi.yml up -d
+docker-compose -f docker-compose.multi.yml stop
 
 echo " ->Deleting snapshot data..."
 rm -rf latest-snapshot/*
 
 echo " ->Downloading latest snapshot"
-curl -O https://download.nine-chronicles.com/latest/9c-main-snapshot.zip
+curl -O $SNAPSHOT_URL
 
 echo " ->Moving zip file"
 mv 9c-main-snapshot.zip latest-snapshot/9c-main-snapshot.zip
@@ -20,18 +22,47 @@ unzip 9c-main-snapshot.zip
 rm 9c-main-snapshot.zip
 
 echo " ->Copying snapshot data"
-sudo cp -r ./* /var/lib/docker/volumes/c9-docker-compose_9c-miner1-volume/_data/
 
-sudo cp -r ./* /var/lib/docker/volumes/c9-docker-compose_9c-miner2-volume/_data/
+MOUNT_LOCATION=$(docker volume inspect --format '{{ .Mountpoint }}' c9dockercompose_9c-miner1)
 
-sudo cp -r ./* /var/lib/docker/volumes/c9-docker-compose_9c-miner3-volume/_data/
+echo " -> Copying to: $MOUNT_LOCATION"
+sudo cp -r ./* $MOUNT_LOCATION
 
-sudo cp -r ./* /var/lib/docker/volumes/c9-docker-compose_9c-miner4-volume/_data/
+MOUNT_LOCATION=$(docker volume inspect --format '{{ .Mountpoint }}' c9dockercompose_9c-miner2)
 
-sudo cp -r ./* /var/lib/docker/volumes/c9-docker-compose_9c-miner5-volume/_data/
+echo " -> Copying to: $MOUNT_LOCATION"
+sudo cp -r ./* $MOUNT_LOCATION
+
+MOUNT_LOCATION=$(docker volume inspect --format '{{ .Mountpoint }}' c9dockercompose_9c-miner3)
+
+echo " -> Copying to: $MOUNT_LOCATION"
+sudo cp -r ./* $MOUNT_LOCATION
+
+MOUNT_LOCATION=$(docker volume inspect --format '{{ .Mountpoint }}' c9dockercompose_9c-miner4)
+
+echo " -> Copying to: $MOUNT_LOCATION"
+sudo cp -r ./* $MOUNT_LOCATION
+
+MOUNT_LOCATION=$(docker volume inspect --format '{{ .Mountpoint }}' c9dockercompose_9c-miner5)
+
+echo " -> Copying to: $MOUNT_LOCATION"
+sudo cp -r ./* $MOUNT_LOCATION
+
+MOUNT_LOCATION=$(docker volume inspect --format '{{ .Mountpoint }}' c9dockercompose_9c-miner6)
+
+echo " -> Copying to: $MOUNT_LOCATION"
+sudo cp -r ./* $MOUNT_LOCATION
 
 echo "->Refresh complete."
 
-echo "->To begin mining, run: docker-compose up -d"
+echo "====================================================="
+echo "->To begin mining, run: docker-compose -f docker-compose.multi.yml up -d"
 echo "->To monitor for mined blocks, run:"
-echo "->docker-compose logs --tail=1 -f | grep -A 10 --color -i 'Mined a block'"
+echo "->docker-compose -f docker-compose.multi.yml logs --tail=1 -f | grep -A 10 --color -i 'Mined a block'"
+echo " " 
+echo "->To monitor ALL log messages:"
+echo "->docker-compose -f docker-compose.multi.yml logs --tail=1 -f"
+echo " "
+echo "->To monitor a single miner:"
+echo "->docker-compose -f docker-compose.multi.yml logs --tail=1 -f 9c-miner1"
+echo "====================================================="
--- refresh-snapshot.sh
+++ refresh-snapshot.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+source .env
+echo $SNAPSHOT_URL
 
 echo " ->Creating a clean environment (delete/start/stop containers)"
 docker-compose down -v
@@ -9,7 +11,7 @@ echo " ->Deleting snapshot data..."
 rm -rf latest-snapshot/*
 
 echo " ->Downloading latest snapshot"
-curl -O https://download.nine-chronicles.com/latest/9c-main-snapshot.zip
+curl -O $SNAPSHOT_URL
 
 echo " ->Moving zip file"
 mv 9c-main-snapshot.zip latest-snapshot/9c-main-snapshot.zip
@@ -21,10 +23,16 @@ rm 9c-main-snapshot.zip
 
 # It would be much better to look for the proper volumes and grab the path with inspect
 echo " ->Copying snapshot data"
-sudo cp -r ./* /var/lib/docker/volumes/c9-docker-compose_9c-miner1-volume/_data/
+MOUNT_LOCATION=$(docker volume inspect --format '{{ .Mountpoint }}' c9dockercompose_9c-miner1)
+sudo cp -r ./* $MOUNT_LOCATION
 
 echo "->Refresh complete."
 
+echo "====================================================="
 echo "->To begin mining, run: docker-compose up -d"
 echo "->To monitor for mined blocks, run:"
 echo "->docker-compose logs --tail=1 -f | grep -A 10 --color -i 'Mined a block'"
+echo " " 
+echo "->To monitor ALL log messages:"
+echo "->docker-compose logs --tail=1 -f"
+echo "====================================================="
